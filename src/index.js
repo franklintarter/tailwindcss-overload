8ba2eaa3e5214653b92112774d0b5ad0bad108f8
Franklin Tarter <franklintarter@gmail.com>
2019-10-14 21:43:50 -0700

    helpful error message when defaultClass props forgotten

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ export const overloadTailwind = (initialClasses, overloadClasses) => {
 
 export const withTailwindOverload = PassedComponent => {
   return ({ className, ...rest }) => {
+    if (!PassedComponent.defaultClassName) {
+      throw new Error(
+        "No defaultClassName found. Components wrapped withTailwindOverload() require defaultClassName property 'MyComponent.defaultClassname = \"text-gray-500\"'"
+      );
+    }
     const overloadedClasses = overloadTailwind(
       PassedComponent.defaultClassName,
       className
